0a34bc0f49f2a93a3720c89a2d682bf01c3ec107
shivansh1907 <dinku.cool19@gmail.com>
2025-06-09 00:45:59 +0530

    production set up

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+
+// function connectDB(){}
+
+
+// connectDB
+
+// instead of this we will use IFFE
+
+;( async ()=>{
+    try{
+        mongoose.connect(`$`)
+    }
+
+})()
